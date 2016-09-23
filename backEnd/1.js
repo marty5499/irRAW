829e8e5844782eabe99aa056d99d9740fe282874
marty <marty.shu@gmail.com>
2016-09-23 13:30:53 +0800

    fix device id

--- backEnd/1.js
+++ backEnd/1.js
@@ -1,6 +1,6 @@
 var webduino = require("webduino-js");
 var led;
-var board = new webduino.WebArduino('mVPL');
+var board = new webduino.WebArduino('GLK7');
 board.on("ready", function(board) {
     led = new webduino.module.Led(board, board.getDigitalPin(13));
     led.blink(100);
